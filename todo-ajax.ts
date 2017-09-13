22bd981b555fa9c626b9cf988a4e906a03aeb45e
selma <selma@gmail.com>
2017-09-13 09:44:02 +0200

    todolist avec typescript et jquery

---
+++ todo-ajax.ts
@@ -0,0 +1,12 @@
+import * as $ from 'jquery';
+
+export class TodoAjax {
+    private urlApi:string = 'http://localhost:3000/todo';
+
+    getAllTodos():Promise<string[]> {
+        return $.ajax({
+            url: this.urlApi
+        });
+    }
+
+}
